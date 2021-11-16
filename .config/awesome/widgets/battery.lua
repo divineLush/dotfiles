d95d894a997b47851e55eb774933113fbb80b370
divineLush <alvipath@yahoo.com>
2021-11-16 02:17:42 -0500

    widget bugfixes

--- .config/awesome/widgets/battery.lua
+++ .config/awesome/widgets/battery.lua
@@ -16,23 +16,26 @@ widget:set_widget(text)
 -- widget:set_bg("#008800")
 -- widget:set_fg("#ffffff")
 
-local was_notified = false
-
 watch("acpi -b", 10, function(widget, stdout, stderr, exitreason, exitcode)
         local val = string.match(stdout, "%d%d")
+        local is_100 = string.find(stdout, "100%%") ~= nil
+
+        if is_100 then
+            val = "100"
+        end
+
         local msg = "bat: "..val
         text:set_text(msg)
 
         local not_charging = string.find(stdout, "Charging") == nil
-        local is_bat_low = tonumber(val) < 40
-
-        if (not not_charging) then
-            widget:set_fg("#c6c8d1")
-        end
+        local is_bat_low = tonumber(val) < 21
+        local color = "#c6c8d1"
 
         if (not_charging) and (is_bat_low) then
-            widget:set_fg("#e27878")
+            color = "#e27878"
         end
+
+        widget:set_fg(color)
     end,
     widget
     )
--- .config/awesome/widgets/volume.lua
+++ .config/awesome/widgets/volume.lua
@@ -18,8 +18,14 @@ widget:set_widget(text)
 
 local function set_widget()
     awful.spawn.easy_async("amixer", function(out)
-        local val = string.match(out, "%p%d%d%p%p")
+        local val = string.match(out, "%[%d%d%%%]")
         local raw_val = string.sub(val, 2, -3)
+
+        local is_100 = string.find(out, "%[100%%") ~= nil
+        if is_100 then
+            raw_val = "100"
+        end
+
         text:set_text("vol: "..raw_val)
     end)
 end

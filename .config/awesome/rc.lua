4d9e20c9ffa268a30707c75bcecd51e418f22ce7
divineLush <alvipath@yahoo.com>
2022-03-15 11:10:57 -0400

    migrate to alacritty

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -61,7 +61,7 @@ end
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "kitty"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 

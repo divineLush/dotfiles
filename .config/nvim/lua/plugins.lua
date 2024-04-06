53e5b7140801238a4e3af78d9bba0f9ce337c8d6
divineLush <alvipath@yahoo.com>
2024-04-06 17:48:44 +0700

    terminal tranpsarency

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -26,6 +26,9 @@ local plugins = {
     name = 'catppuccin',
     priority = 1000,
     config = function ()
+      require'catppuccin'.setup({
+        transparent_background = true,
+      })
       vim.cmd.colorscheme 'catppuccin-mocha'
     end
   },
